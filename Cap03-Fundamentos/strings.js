ee8fbb5c7f2a41959deed5946162569a74ee97fc
Henrique Marques <hfvmarques@gmail.com>
2021-07-16 10:11:10 -0400

    Strings

---
+++ Cap03-Fundamentos/strings.js
@@ -0,0 +1,16 @@
+const escola = 'Cod3r'
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3)) // Código na tabela unicode
+console.log(escola.indexOf('d'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat('!'))
+console.log('Escola ' + escola + '!') // Alternativa para concatenar conteúdo
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
+console.log('Ana Maria Pedro'.split(' '))
