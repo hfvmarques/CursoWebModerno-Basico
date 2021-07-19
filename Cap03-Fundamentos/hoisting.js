5468bb503ecc9adcdb2d68415396b6ee1850d49f
Henrique Marques <hfvmarques@gmail.com>
2021-07-18 21:29:11 -0400

    Hoisting

---
+++ Cap03-Fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+// o código acima é o mesmo como se estivesse igual abaixo
+// var a
+// console.log('a =', a)
+// a = 2
+// console.log('a =', a)
+
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
