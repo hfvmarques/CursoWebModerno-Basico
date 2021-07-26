467d7636ad506d19cc2a4456b63c6e3046d00892
Henrique Marques <hfvmarques@gmail.com>
2021-07-26 15:26:17 -0400

    Metodos importantes de array

---
+++ Cap07-Array/arrayMetodos.js
@@ -0,0 +1,25 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o último elemento
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') // insere um elemento na pisição 0
+console.log(pilotos)
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+pilotos.splice(3, 1)
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // cria um novo array
+
+console.log(algunsPilotos1)
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
