4bfaf32690464d717595830f02dcad0223e5fcd8
Henrique Marques <hfvmarques@gmail.com>
2021-07-27 10:58:38 -0400

    Filter 2

---
+++ Cap07-Array/filter2.js
@@ -0,0 +1,21 @@
+Array.prototype.filter2 = function (callback) {
+  const newArray = []
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i])
+    }
+  }
+  return newArray
+}
+
+const produtos = [
+  { nome: 'Notebook', preco: 2499, fragil: true },
+  { nome: 'iPad Pro', preco: 4199, fragil: true },
+  { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+  { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const produtoCaro = p => p.preco >= 500
+const produtoFragil = p => p.fragil == true // o true é opcional
+
+console.log(produtos.filter2(produtoFragil).filter2(produtoCaro))
